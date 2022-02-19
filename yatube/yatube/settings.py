76aa6c7a983b5a7b6b76e9ab2e8182abf011a053
Danstiv <50794055+Danstiv@users.noreply.github.com>
2022-02-19 22:30:40 +0700

    Include django-debug-toolbar

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -47,9 +47,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +60,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("application/javascript", ".js", True)
 
 ROOT_URLCONF = 'yatube.urls'
 ABSOLUTE_URL_OVERRIDES = {
